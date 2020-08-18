16c2d2a7ba17d40930134b857fc4594289e734b2
React <chenchunyen0727@gmail.com>
2020-08-19 00:51:01 +0800

    Update unit.js

--- unit.js
+++ unit.js
@@ -1,8 +1,36 @@
 const should = require('should');
 import JSONReverse from './JSONReverse';
 
+let inputValue = {
+  hired: {
+    be: {
+      to: {
+        deserve: 'I'
+      }
+    }
+  }
+};
+
+let outputValue = {
+  I: {
+    deserve: {
+      to: {
+         be: 'hired'
+      }
+    }
+  }
+};
+
 describe('unit testing', () => {
   it('should return "input data type fail"', done => {
-    JSONReverse().should.equal(null)
+    JSONReverse().should.equal('input data type fail')
+  })
+  
+  it('should return "nothing in object"', done => {
+    JSONReverse({}).should.equal('nothing in object')
+  })
+  
+  it('should return outputValue', done => {
+    JSONReverse(inputValue).should.equal(outputValue)
   })
 })
