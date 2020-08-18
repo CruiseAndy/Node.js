5246387c94aa615e3dd7df2ecc1cacc3b43630cf
React <chenchunyen0727@gmail.com>
2020-08-19 00:18:40 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,45 @@
 
-let reverseRlt = null;
+let reverseRlt = {};
 let tmpArr = [];
 
 const JSONReverse = obj => {
-  if (obj == null || typeof(obj) == 'undefined' || !Array.isArray(obj))
+  if (obj == null || typeof(obj) == 'undefined' || Array.isArray(obj))
     return 'input data type fail';
     
   for (let key in obj) {
-    if (obj.hasOwnProperty(key))
+    if (!obj.hasOwnProperty(key))
       return 'nothing in object';
   }
   
   exeReverse(obj);
   
+  reverseRlt = { [tmpArr[1]]: tmpArr[0] }
+  for(let index = 2; index < tmpArr.length; index++) {
+    reverseRlt = { [tmpArr[index]]: reverseRlt }
+  }
+  
+  console.log(reverseRlt);
+}
+
+const exeReverse = obj => {
+  let tmpData = Object.entries(obj)[0];
+  tmpArr.push(tmpData[0])
+  
+  if (typeof(tmpData[1]) == 'object') {
+    exeReverse(tmpData[1])
+  }
+  else {
+    tmpArr.push(tmpData[1])
+  }
+  
+//   if (typeof(tmpData[1] == 'object') {
+//       return { [exeReverse(tmpData[1])]: tmpData[0] }
+//   }
+  
+//   if (typeof(tmpData[1]) != 'object') {
+//     return { [tmpData[1]]: tmpData[0] };
+//   }
+//   else {
+//     return { [exeReverse(tmpData[1])]: tmpData[0] }
+//   }
 }
